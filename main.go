b3aae0462f8eeeb2542ea621875071443ac2933a
Johannes Meshchain <khairuldinsuyitno@gmail.com>
2026-02-18 17:25:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -272,7 +272,7 @@ func main() {
     go func() {
         port := os.Getenv("PORT")
         if port == "" {
-            port = "8000"
+            port = "8080"
         }
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
             fmt.Fprintf(w, "✅ Cryptorian Bot is Running Live!")
